60ab67eda79ca35bcb2e543fd416d5fd39cc4484
Ricardo Pérez <ricardo@ubuntu.com>
2016-01-12 20:01:34 +0100

    Carga inicial

---
+++ application/controllers/Articulos.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Articulos extends CI_Controller
+{
+    public function index()
+    {
+        $data['filas'] = $this->db->query('select * from articulos')->result_array();
+        $this->load->view('articulos/index', $data);
+    }
+
+    public function borrar($id)
+    {
+        $this->db->query("delete from articulos where id = ?", array($id));
+        redirect('articulos/index');
+    }
+}
