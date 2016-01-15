ad6574dc7711c3c17f0d04ea2468706c80cabfb6
Ricardo Pérez <ricardo@ubuntu.com>
2016-01-15 17:57:06 +0100

    Primer intento de validación de formularios

--- application/controllers/Articulos.php
+++ application/controllers/Articulos.php
@@ -8,6 +8,51 @@ class Articulos extends CI_Controller
         $this->load->view('articulos/index', $data);
     }
 
+    public function insertar()
+    {
+        if ($this->input->post('insertar') !== NULL)
+        {
+            $reglas = array(
+                array(
+                    'field' => 'codigo',
+                    'label' => 'Código',
+                    'rules' => 'trim|required|numeric|max_length[13]'
+                ),
+                array(
+                    'field' => 'descripcion',
+                    'label' => 'Descripción',
+                    'rules' => 'trim|required|max_length[50]'
+                ),
+                array(
+                    'field' => 'precio',
+                    'label' => 'Precio',
+                    'rules' => 'trim|required|decimal|less_than_equal_to[9999.99]'
+                ),
+                array(
+                    'field' => 'existencias',
+                    'label' => 'Existencias',
+                    'rules' => 'trim|integer|greather_than_equal_to[-2147483648]|less_than_equal_to[+2147483647]'
+                )
+            );
+            $this->form_validation->set_rules($reglas);
+            if ($this->form_validation->run() === FALSE)
+            {
+                $this->load->view('articulos/insertar');
+            }
+            else
+            {
+                $valores = $this->input->post();
+                unset($valores['insertar']);
+                $this->Articulo->insertar($valores);
+                redirect('articulos/index');
+            }
+        }
+        else
+        {
+            $this->load->view('articulos/insertar');
+        }
+    }
+
     public function borrar($id = NULL)
     {
         if ($this->input->post('borrar') !== NULL)
