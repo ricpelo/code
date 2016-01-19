b35defa07d8b2bc9873b02231117929e9e1dc8ff
Ricardo Pérez <ricpelo@gmail.com>
2016-01-19 22:10:48 +0100

    Pequeños retoques estéticos

--- application/views/articulos/editar.php
+++ application/views/articulos/editar.php
@@ -1,37 +1,46 @@
 <?php template_set('title', 'Editar un artículo') ?>
-<div class="container-fluid">
+<div class="container-fluid" style="padding-top:20px">
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
-      <?= validation_errors() ?>
-      <?= form_open("articulos/editar/$id") ?>
-        <div class="form-group">
-          <?= form_label('Código:', 'codigo') ?>
-          <?= form_input('codigo',
-                         set_value('codigo', $codigo, FALSE),
-                         'id="codigo" class="form-control"') ?>
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h3 class="panel-title">Editar artículo</h3>
+          </div>
+          <div class="panel-body">
+            <?= validation_errors('<div class="alert alert-danger" role="alert">',
+                                  '</div>') ?>
+            <?= form_open("articulos/editar/$id") ?>
+              <div class="form-group">
+                <?= form_label('Código:', 'codigo') ?>
+                <?= form_input('codigo',
+                               set_value('codigo', $codigo, FALSE),
+                               'id="codigo" class="form-control"') ?>
+              </div>
+              <div class="form-group">
+                <?= form_label('Descripción:', 'descripcion') ?>
+                <?= form_input('descripcion',
+                               set_value('descripcion', $descripcion, FALSE),
+                               'id="descripcion" class="form-control"') ?>
+              </div>
+              <div class="form-group">
+                <?= form_label('Precio:', 'precio') ?>
+                <?= form_input('precio',
+                               set_value('precio', $precio, FALSE),
+                               'id="precio" class="form-control"') ?>
+              </div>
+              <div class="form-group">
+                <?= form_label('Existencias:', 'existencias') ?>
+                <?= form_input('existencias',
+                               set_value('existencias', $existencias, FALSE),
+                               'id="existencias" class="form-control"') ?>
+              </div>
+              <?= form_submit('editar', 'Editar', 'class="btn btn-success"') ?>
+              <?= anchor('/articulos/index', 'Cancelar',
+                         'class="btn btn-danger" role="button"') ?>
+            <?= form_close() ?>
+          </div>
         </div>
-        <div class="form-group">
-          <?= form_label('Descripción:', 'descripcion') ?>
-          <?= form_input('descripcion',
-                         set_value('descripcion', $descripcion, FALSE),
-                         'id="descripcion" class="form-control"') ?>
-        </div>
-        <div class="form-group">
-          <?= form_label('Precio:', 'precio') ?>
-          <?= form_input('precio',
-                         set_value('precio', $precio, FALSE),
-                         'id="precio" class="form-control"') ?>
-        </div>
-        <div class="form-group">
-          <?= form_label('Existencias:', 'existencias') ?>
-          <?= form_input('existencias',
-                         set_value('existencias', $existencias, FALSE),
-                         'id="existencias" class="form-control"') ?>
-        </div>
-        <?= form_submit('editar', 'Editar', 'class="btn btn-success"') ?>
-        <?= anchor('/articulos/index', 'Cancelar',
-                   'class="btn btn-danger" role="button"') ?>
-      <?= form_close() ?>
+      </div>
     </div>
   </div>
 </div>
