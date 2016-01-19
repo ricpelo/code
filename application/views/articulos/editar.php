2ef59697eaacc068035c2b5fc3c81f5462ea57da
Ricardo Pérez <ricardo@ubuntu.com>
2016-01-19 20:31:41 +0100

    Incluye estilos de Bootstrap

--- application/views/articulos/editar.php
+++ application/views/articulos/editar.php
@@ -1,13 +1,32 @@
-<?= validation_errors() ?>
-<?= form_open("articulos/editar/$id") ?>
-    <?= form_label('Código:', 'codigo') ?>
-    <?= form_input('codigo', set_value('codigo', $codigo, FALSE), 'id="codigo"') ?><br/>
-    <?= form_label('Descripción:', 'descripcion') ?>
-    <?= form_input('descripcion', set_value('descripcion', $descripcion, FALSE), 'id="descripcion"') ?><br/>
-    <?= form_label('Precio:', 'precio') ?>
-    <?= form_input('precio', set_value('precio', $precio, FALSE), 'id="precio"') ?><br/>
-    <?= form_label('Existencias:', 'existencias') ?>
-    <?= form_input('existencias', set_value('existencias', $existencias, FALSE), 'id="existencias"') ?><br/>
-    <?= form_submit('editar', 'Editar') ?>
-    <?= anchor('/articulos/index', form_button('cancelar', 'Cancelar')) ?>
-<?= form_close() ?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <?= validation_errors() ?>
+      <?= form_open("articulos/editar/$id") ?>
+        <div class="form-group">
+          <?= form_label('Código:', 'codigo') ?>
+          <?= form_input('codigo', set_value('codigo', $codigo, false),
+                   'id="codigo" class="form-control"') ?>
+        </div>
+        <div class="form-group">
+          <?= form_label('Descripción:', 'descripcion') ?>
+          <?= form_input('descripcion', set_value('descripcion', $descripcion, false),
+                   'id="descripcion" class="form-control"') ?>
+        </div>
+        <div class="form-group">
+          <?= form_label('Precio:', 'precio') ?>
+          <?= form_input('precio', set_value('precio', $precio, false),
+                   'id="precio" class="form-control"') ?>
+        </div>
+        <div class="form-group">
+          <?= form_label('Existencias:', 'existencias') ?>
+          <?= form_input('existencias', set_value('existencias', $existencias, false),
+                   'id="existencias" class="form-control"') ?>
+        </div>
+        <?= form_submit('editar', 'editar', 'class="btn btn-success"') ?>
+        <?= anchor('/articulos/index', 'cancelar',
+               'class="btn btn-danger" role="button"') ?>
+      <?= form_close() ?>
+    </div>
+  </div>
+</div>
--- application/views/articulos/index.php
+++ application/views/articulos/index.php
@@ -1,22 +1,34 @@
-<table border="1">
-    <thead>
-        <th>Código</th>
-        <th>Descripción</th>
-        <th colspan="2">Acciones</th>
-    </thead>
-    <tbody>
-        <?php foreach ($filas as $fila): ?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <table border="1"
+             class="table table-striped table-bordered table-hover table-condensed">
+        <thead>
+          <th>Código</th>
+          <th>Descripción</th>
+          <th colspan="2">Acciones</th>
+        </thead>
+        <tbody>
+          <?php foreach ($filas as $fila): ?>
             <tr>
-                <td><?= $fila['codigo'] ?></td>
-                <td><?= $fila['descripcion'] ?></td>
-                <td><?= anchor('/articulos/borrar/' . $fila['id'],
-                               form_button('borrar', 'Borrar')) ?></td>
-                <td><?= anchor('/articulos/editar/' . $fila['id'],
-                               form_button('editar', 'Editar')) ?></td>
+              <td><?= $fila['codigo'] ?></td>
+              <td><?= $fila['descripcion'] ?></td>
+              <td align="center">
+                <?= anchor('/articulos/borrar/' . $fila['id'], 'Borrar',
+                           'class="btn btn-danger btn-xs" role="button"') ?>
+              </td>
+              <td align="center">
+                <?= anchor('/articulos/editar/' . $fila['id'], 'Editar',
+                           'class="btn btn-warning btn-xs" role="button"') ?>
+              </td>
             </tr>
-        <?php endforeach ?>
-    </tbody>
-</table>
-<p>
-    <?= anchor('articulos/insertar', form_button('insertar', 'Insertar')) ?>
-</p>
+          <?php endforeach ?>
+        </tbody>
+      </table>
+      <p>
+        <?= anchor('articulos/insertar', 'Insertar',
+                   'class="btn btn-success" role="button"') ?>
+      </p>
+    </div>
+  </div>
+</div>
