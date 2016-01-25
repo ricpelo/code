76707b93577981ea2eb67c413374b55e5786bc38
Ricardo Pérez <ricardo@ubuntu.com>
2016-01-25 17:19:19 +0100

    Primeros pasos con el controlador de Usuarios

--- application/sql/datos.sql
+++ application/sql/datos.sql
@@ -10,11 +10,11 @@
 -- USUARIOS --
 
     insert into usuarios (nick, password, rol_id)
-                         values ('admin', md5('admin'), 1),
-                                ('roberto', md5('roberto'), 2),
-                                ('pepe', md5('pepe'), 2),
-                                ('juan', md5('juan'), 2),
-                                ('maria', md5('maria'), 2);
+                         values ('admin', crypt('admin', gen_salt('bf')), 1),
+                                ('roberto', crypt('roberto', gen_salt('bf')), 2),
+                                ('pepe', crypt('pepe', gen_salt('bf')), 2),
+                                ('juan', crypt('juan', gen_salt('bf')), 2),
+                                ('maria', crypt('maria', gen_salt('bf')), 2);
 
 
 -- ARTICULOS --
@@ -55,13 +55,13 @@
 
 -- PEDIDOS --
 
-    insert into pedidos (numero, fecha, cliente_id, codigo, 
+    insert into pedidos (numero, fecha, cliente_id, codigo,
                          nombre, apellidos, dni, direccion, poblacion, codigo_postal)
                          values (14000001, current_date-4, 1, 110001, 'Roberto', 'López Gómez', '31224553H',
                                  'Avda. de Compostela, 34', 'Sanlúcar de Barrameda', '11540'),
                                 (14000002, current_date-3, 1, 110001, 'Roberto', 'López Gómez', '31224553H',
                                  'Avda. de Compostela, 34', 'Sanlúcar de Barrameda', '11540'),
-                                (14000003, current_date-2, 3, 410001, 'Juan', 'Rey Sanjosé', '48997345F', 
+                                (14000003, current_date-2, 3, 410001, 'Juan', 'Rey Sanjosé', '48997345F',
                                  'Avda. República Argentina, 45 Alto', 'Sevilla', '41003'),
                                 (14000004, current_date-1, 4, 410002, 'María José', 'Moreno Luque', '41823099P',
                                  'Avda. de los Conquistadores, 44', 'Dos Hermanas', '41234');
@@ -70,10 +70,9 @@
 
     insert into lineas_pedidos (pedido_id, codigo, descripcion, precio, cantidad)
                        values (1, 8499786675192, 'Ibook Sandbook 7" e-ink WIFI-3G 16GB', 99.99, 1),
-                              (1, 8433326765499, 'Monitor ACER 27" 2560x1980 pix. 3D', 255.00, 2), 
+                              (1, 8433326765499, 'Monitor ACER 27" 2560x1980 pix. 3D', 255.00, 2),
                               (1, 8433556543445, 'Disco duro Seagate Expansion 2 TB - 2,5", USB 3.0', 80.45, 2),
-                              (2, 2887942650087, 'Portátil Lenovo 13,3" YOGA 2 Pro-13 i7 4500U', 480.45, 1), 
+                              (2, 2887942650087, 'Portátil Lenovo 13,3" YOGA 2 Pro-13 i7 4500U', 480.45, 1),
                               (2, 8433556543445, 'Disco duro Seagate Expansion 2 TB - 2,5", USB 3.0', 80.45, 2),
                               (3, 8499786675192, 'Ibook Sandbook 7" e-ink WIFI-3G 16GB', 99.99, 1),
                               (4, 8433556543445, 'Disco duro Seagate Expansion 2 TB - 2,5", USB 3.0', 80.45, 5);
-
