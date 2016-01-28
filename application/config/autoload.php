9479680d972b4a62f27e64d8dc980b7f7ac17229
Ricardo Pérez <ricardo@ubuntu.com>
2016-01-28 19:14:26 +0100

    Bootstrap local

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -83,7 +83,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'usuario');
+$autoload['helper'] = array('url', 'html', 'form', 'usuario');
 
 /*
 | -------------------------------------------------------------------
