55a38d953899f8607a8764f54caa3ccf6cdcf61f
Ricardo Pérez <ricardo@ubuntu.com>
2016-01-28 19:14:26 +0100

    Bootstrap local

--- application/views/template.php
+++ application/views/template.php
@@ -6,15 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= isset($title) ? $title : '' ?></title>
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet"
-              href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-              integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
-              crossorigin="anonymous">
+        <?= link_tag('assets/css/bootstrap.min.css') ?>
         <!-- Optional theme -->
-        <link rel="stylesheet"
-              href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
-              integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
-              crossorigin="anonymous">
+        <?= link_tag('assets/css/bootstrap-theme.min.css') ?>
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
@@ -26,10 +20,8 @@
         <?= login() ?>
         <?= $contents ?>
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="<?= base_url('assets/js/jquery-2.2.0.min.js') ?>"></script>
         <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
-                integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-                crossorigin="anonymous"></script>
+        <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
     </body>
 </html>
