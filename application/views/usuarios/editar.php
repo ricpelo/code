792ed90d21bb913316866f022140bcba6eef9e4d
Mario Sumariva <mariosumd@gmail.com>
2016-02-02 23:51:31 +0100

    Añade el editar de usuarios

--- application/views/usuarios/editar.php
+++ application/views/usuarios/editar.php
@@ -1,45 +1,49 @@
-<?php template_set('title', 'Editar un artículo') ?>
-<div class="row">
-  <div class="col-md-8 col-md-offset-2">
-    <div class="panel panel-primary">
-      <div class="panel-heading">
-        <h3 class="panel-title">Editar artículo</h3>
-      </div>
-      <div class="panel-body">
-        <?php if ( ! empty(error_array())): ?>
-          <div class="alert alert-danger" role="alert">
-            <?= validation_errors() ?>
-          </div>
-        <?php endif ?>
-        <?= form_open("articulos/editar/$id") ?>
-          <div class="form-group">
-            <?= form_label('Código:', 'codigo') ?>
-            <?= form_input('codigo',
-                           set_value('codigo', $codigo, FALSE),
-                           'id="codigo" class="form-control"') ?>
-          </div>
-          <div class="form-group">
-            <?= form_label('Descripción:', 'descripcion') ?>
-            <?= form_input('descripcion',
-                           set_value('descripcion', $descripcion, FALSE),
-                           'id="descripcion" class="form-control"') ?>
-          </div>
-          <div class="form-group">
-            <?= form_label('Precio:', 'precio') ?>
-            <?= form_input('precio',
-                           set_value('precio', $precio, FALSE),
-                           'id="precio" class="form-control"') ?>
-          </div>
-          <div class="form-group">
-            <?= form_label('Existencias:', 'existencias') ?>
-            <?= form_input('existencias',
-                           set_value('existencias', $existencias, FALSE),
-                           'id="existencias" class="form-control"') ?>
-          </div>
-          <?= form_submit('editar', 'Editar', 'class="btn btn-success"') ?>
-          <?= anchor('/articulos/index', 'Cancelar',
-                     'class="btn btn-danger" role="button"') ?>
-        <?= form_close() ?>
+<?php template_set('title', 'Editar un usuario') ?>
+<div class="container-fluid" style="padding-top:20px">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">Editar usuario</h3>
+        </div>
+        <div class="panel-body">
+          <?php if ( ! empty(error_array())): ?>
+            <div class="alert alert-danger" role="alert">
+              <?= validation_errors() ?>
+            </div>
+          <?php endif ?>
+          <?= form_open("usuarios/editar/$id") ?>
+            <div class="form-group">
+              <?= form_label('Nick:', 'nick') ?>
+              <?= form_input('nick',
+                             set_value('nick', $nick, FALSE),
+                             'id="nick" class="form-control"') ?>
+            </div>
+            <div class="form-group">
+              <?= form_label('Contraseña Antigua:', 'password_anterior') ?>
+              <?= form_password('password_anterior', '',
+                             'id="password_anterior" class="form-control"') ?>
+            </div>
+            <div class="form-group">
+              <?= form_label('Contraseña:', 'password') ?>
+              <?= form_password('password', '',
+                             'id="password" class="form-control"') ?>
+            </div>
+            <div class="form-group">
+              <?= form_label('Confirmar Contraseña:', 'password_confirm') ?>
+              <?= form_password('password_confirm', '',
+                             'id="password_confirm" class="form-control"') ?>
+            </div>
+            <div class="form-group">
+              <?= form_label('Rol:', 'rol_id') ?>
+              <?= form_dropdown('rol_id', $roles, set_value('rol_id', $rol_id, FALSE),
+                                'id="password" class="form-control"') ?>
+            </div>
+            <?= form_submit('editar', 'Editar', 'class="btn btn-success"') ?>
+            <?= anchor('/usuarios/index', 'Cancelar',
+                       'class="btn btn-danger" role="button"') ?>
+          <?= form_close() ?>
+        </div>
       </div>
     </div>
   </div>
